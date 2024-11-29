e21b08faccbf975c4d23917ced375b19bff0a408
Urga7 <urban.gajsek@gmail.com>
2024-11-29 18:11:26 +0100

    gitignore fixing

--- homework_2_MM1/out/clang-release/P2PNetwork.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//P2PNetwork.o: P2PNetwork.cc P2PNetwork.h \
-  C:/Programs/omnetpp-6.1-windows-x86_64/omnetpp-6.1/include/omnetpp.h
-
-P2PNetwork.h:
-
-C:/Programs/omnetpp-6.1-windows-x86_64/omnetpp-6.1/include/omnetpp.h:
--- mm1/out/clang-debug/mySink.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-debug//mySink.o: mySink.cc mySink.h \
-  C:/Programs/omnetpp-6.1-windows-x86_64/omnetpp-6.1/include/omnetpp.h
-
-mySink.h:
-
-C:/Programs/omnetpp-6.1-windows-x86_64/omnetpp-6.1/include/omnetpp.h:
